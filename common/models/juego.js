1ad3289216d15c534e9e28084d65400c7c8bce72
Javi <1832517@alu.murciaeduca.es>
2018-10-08 09:07:03 +0200

    GetName de juegos creado

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Juego) {
+/**
+ * Devuelve el nombre del Juego
+ * @param {Function(Error, string)} callback
+ */
 
+Juego.prototype.getNombre = function(callback) {  
+  callback(null, this.nombre);
+};
 };
